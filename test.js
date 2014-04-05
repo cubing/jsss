935fafb51e06d55509e6ca29e8f0d65d53c25195
Nick Colley <nickcolley7@gmail.com>
2014-04-05 22:08:59 +0100

    Added test

--- test.js
+++ test.js
@@ -3,7 +3,9 @@
 var Assert = require('assert');
 var Scramble = require('./scramble.js');
 
-// Type should be set to 333
 var test = new Scramble();
 test.type('333');
 Assert.equal(test.type(), '333', 'Type should be set to 333');
+
+var test = new Scramble();
+Assert.equal(test.type(), '333', 'Default type should be 333');
