9bee284d6aef31cb8c57e15f33afd40f5199ef78
Lucas Garron <code@garron.net>
2020-12-27 19:02:58 -0800

    Document events in `README.md`.

--- src/scrambles/api/outside.ts
+++ src/scrambles/api/outside.ts
@@ -65,21 +65,3 @@ export async function randomScrambleStringForEvent(
   }
   return algToString(await _randomScrambleForEvent(eventID));
 }
-
-// 333
-// 222
-// 444
-// 555
-// 666
-// 777
-// 333bf
-// 333fm
-// 333oh
-// clock
-// minx
-// pyram
-// skewb
-// sq1
-// 444bf
-// 555bf
-// 333mbf
